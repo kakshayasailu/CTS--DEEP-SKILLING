9e94d58a66c3ce348248f5fc508562617768f88f
kakshayasailu <akshaya.karpuram165@gmail.com>
2026-07-23 13:22:07 +0530

    Add files via upload

---
+++ Module 4 NUnit&Moq/CustomerCommLib/CustomerComm.Tests/CustomerCommTest.cs
@@ -0,0 +1,33 @@
+﻿using NUnit.Framework;
+using Moq;
+using CustomerCommLib;
+
+namespace CustomerComm.Tests
+{
+    [TestFixture]
+    public class CustomerCommTest
+    {
+        private Mock<IMailSender> mockMailSender;
+
+        [OneTimeSetUp]
+        public void Init()
+        {
+            mockMailSender = new Mock<IMailSender>();
+
+            mockMailSender
+                .Setup(x => x.SendMail(It.IsAny<string>(), It.IsAny<string>()))
+                .Returns(true);
+        }
+
+        [TestCase]
+        public void SendMailToCustomer_ShouldReturnTrue()
+        {
+            CustomerCommLib.CustomerComm customerComm =
+                new CustomerCommLib.CustomerComm(mockMailSender.Object);
+
+            bool result = customerComm.SendMailToCustomer();
+
+            Assert.That(result, Is.True);
+        }
+    }
+}
