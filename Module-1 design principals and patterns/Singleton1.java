a5fe0bd5c8336d268b1b8dba52851b67e9726c79
kakshayasailu <akshaya.karpuram165@gmail.com>
2026-07-23 12:56:25 +0530

    Add files via upload

---
+++ Module-1 design principals and patterns/Singleton1.java
@@ -0,0 +1,25 @@
+class Singleton {
+
+    private static Singleton instance;
+
+    private Singleton() {
+        System.out.println("Singleton Created");
+    }
+
+    public static Singleton getInstance() {
+        if (instance == null) {
+            instance = new Singleton();
+        }
+        return instance;
+    }
+}
+
+public class Singleton1{
+    public static void main(String[] args) {
+
+        Singleton s1 = Singleton.getInstance();
+        Singleton s2 = Singleton.getInstance();
+
+        System.out.println(s1 == s2);
+    }
+}
